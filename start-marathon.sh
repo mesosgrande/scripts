ab54546d1481e54a92335ca9e60a42fa7095ffc5
root <root@localhost.localdomain>
2014-11-12 12:14:14 -0500

    add stef zookeeper

--- start-marathon.sh
+++ start-marathon.sh
@@ -1,10 +1,9 @@
 docker run \
   --name marathon \
-  --link zookeeper:zookeeper \
   --link master:master \
   -d \
   -p 8080:8080 \
   mesosgrande/marathon \
   --master master:5050 \
-  --zk zk://zookeeper:2181/marathon
+  --zk zk://172.17.35.196:2181/marathon
 
