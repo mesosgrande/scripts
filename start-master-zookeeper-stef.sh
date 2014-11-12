0fb8546ada3a6c74646ba18653bbb5fa8d26f91a
Stef Corputty <stef@laptop01-2.local>
2014-11-12 17:33:01 +0100

    new file:   start-master-zookeeper-stef.sh 	modified:   start-master.sh

--- start-master-zookeeper-stef.sh
+++ start-master-zookeeper-stef.sh
@@ -6,6 +6,6 @@ docker run \
   -e MESOS_QUORUM=1 \
   -e MESOS_LOG_DIR=/var/log \
   -e MESOS_WORK_DIR=/tmp \
-  -e MESOS_ZK=zk://zookeeper:2181/mesos \
+  -e MESOS_ZK=zk://172.17.35.196:2181/mesos \
   -p 5050:5050 \
   mesosgrande/master
